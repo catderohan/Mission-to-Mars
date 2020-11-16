870feeb0429e861180c8b389b528ef2e82fe52d8
catderohan <cath7489.com>
2020-11-15 22:49:45 -0600

    new files

--- scraping.py
+++ scraping.py
@@ -17,7 +17,8 @@ def scrape_all():
         "news_paragraph": news_paragraph,
         "featured_image": featured_image(browser),
         "facts": mars_facts(),
-        "last_modified": dt.datetime.now()
+        "last_modified": dt.datetime.now(),
+        "hemispheres": hemisphere(browser)
     }
 
     # Stop webdriver and return data
@@ -99,6 +100,42 @@ def mars_facts():
 
     # Convert dataframe into HTML format, add bootstrap
     return df.to_html(classes="table table-striped")
+
+
+
+def hemisphere(browser):
+    # 1. Use browser to visit the URL 
+    url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
+    browser.visit(url)
+
+    # 2. Create a list to hold the images and titles.
+    hemisphere_image_urls = []
+
+    # 3. Write code to retrieve the image urls and titles for each hemisphere.
+    #Get links
+    links = browser.find_by_css('a.product-item h3')
+
+    #create for loop
+    for i in range(len(links)):
+        #Create dictionary of hemisphere to hold titles and string
+        hemisphere = {}
+        #Click on each hemisphere link
+        browser.find_by_css('a.product-item h3')[i].click()
+        #Navigate to the full-resolution image page
+        sample_element = browser.links.find_by_text('Sample').first
+        #Retrieve the full-resolution image URL string 
+        hemisphere['img_url'] = sample_element['href']
+        #Retrieve the full-resolution title
+        hemisphere['title'] = browser.find_by_css('h2.title').text
+        #Append hemisphere object to list
+        hemisphere_image_urls.append(hemisphere)
+        #navigate back
+        browser.back()
+   
+   
+    # 4. Print the list that holds the dictionary of each image url and title.
+    return hemisphere_image_urls
+
 
 if __name__ == "__main__":
 
